4d17f09db5ff86deecc7ad973d6d2f1747c99bf3
francoromeo <francoromeo5012@gmail.com>
2023-11-02 13:10:02 -0300

    home

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!', 'Freelancer!', 'UI Designer!', 'YouTuber!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
