70388bed47972e42040b61607ac1ae09df218072
francoromeo <francoromeo5012@gmail.com>
2023-11-19 18:35:31 -0300

    scroll

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ let typed = new Typed('.auto-input',{
     backDelay: 2000,
     loop: true,
 })
+
+let navLinks = document.querySelectorAll(`nav ul li a`)
+let sections = document.querySelectorAll('section')
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
